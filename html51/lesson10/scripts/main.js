0b9add298ba587ff899aa29b8cf294f9e644f1a3
katyaprodan <57104447+katyaprodan@users.noreply.github.com>
2019-10-31 18:46:27 +0200

    Завдання станом на 31.10

---
+++ html51/lesson10/scripts/main.js
@@ -0,0 +1,6 @@
+function createImage() {
+ var canvas = document.getElementById("my_canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgb(100,0,0)"
+     ctx.fillRect(20, 10, 50, 70);
+    }
