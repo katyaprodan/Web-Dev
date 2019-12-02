3e726cbd4669ddb47b067b7465c990df66c151d3
katyaprodan <57104447+katyaprodan@users.noreply.github.com>
2019-12-02 19:11:49 +0200

    02.12.2019

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+int mas[4]={123};
+printf("%d", mas[0]);
+int mas1[5]={456};
+printf("%d", mas1[0]);
+int mas2[6]={789};
+printf("%d", mas2[0]);
+	return 0;
+}
