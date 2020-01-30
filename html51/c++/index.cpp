167747d19c26bac0c15915d02484090c0f5429e4
katyaprodan <57104447+katyaprodan@users.noreply.github.com>
2020-01-30 18:38:23 +0200

    30.01

---
+++ html51/c++/index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0; i<=9; i++)
+    
+        cout<<i<<'\t';
+        cout<<endl;
+
+    for(int i=1; i<=9; i++)
+    {
+          cout<<i<<'\t';
+     for(int a=1; a<=9)
+     int b=i*a;
+     cout<<b<<'\t';
+     cout<<endl;
+     return 0;     
+    }
+}
